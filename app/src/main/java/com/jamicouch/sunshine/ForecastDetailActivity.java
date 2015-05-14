aa5482ca4d828a40dfa39d1c13de281afa9e18af
Jami Couch <jami.couch@metova.com>
2015-05-14 09:22:27 -0500

    Lesson 3: Sharing

--- app/src/main/java/com/jamicouch/sunshine/ForecastDetailActivity.java
+++ app/src/main/java/com/jamicouch/sunshine/ForecastDetailActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
 
@@ -21,6 +22,7 @@ public class ForecastDetailActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_forecast_detail, menu);
+
         return true;
     }
 
--- app/src/main/java/com/jamicouch/sunshine/ForecastDetailFragment.java
+++ app/src/main/java/com/jamicouch/sunshine/ForecastDetailFragment.java
@@ -3,7 +3,12 @@ package com.jamicouch.sunshine;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,8 +19,16 @@ import android.widget.Toast;
  * A placeholder fragment containing a simple view.
  */
 public class ForecastDetailFragment extends Fragment {
+    private static final String TAG = ForecastDetailFragment.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = "#SunshineApp";
+
+    private String mForecastStr;
+
+    ShareActionProvider mShareActionProvider;
 
     public ForecastDetailFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -24,7 +37,36 @@ public class ForecastDetailFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_forecast_detail, container, false);
 
         TextView detailText = (TextView)rootView.findViewById(R.id.forecast_detail_textview);
-        detailText.setText(getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT));
+        mForecastStr = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
+        detailText.setText(mForecastStr);
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.forecastdetailfragment, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+
+        mShareActionProvider = new ShareActionProvider(getActivity());
+        MenuItemCompat.setActionProvider(shareItem, mShareActionProvider);
+
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET)
+                .setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, mForecastStr + " " + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 }
